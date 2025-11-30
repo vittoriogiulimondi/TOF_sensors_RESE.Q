e856e905f0d92a0c0feff772b28d0777acba672e
vittoriogiulimondi <vittoriogiulimondi@gmail.com>
2025-11-30 21:34:14 +0100

    Commented Pico2zero

--- Pico2Zero/Pico2Zero.ino
+++ Pico2Zero/Pico2Zero.ino
@@ -1,55 +1,52 @@
-/* This is an old PicoLowLevel test code, used only to verify that the Makefile works correctly.
-Do not upload it to the Pico — it may contain errors or modified sections.
-Use it exclusively for Makefile testing. */
+/*
+  This is is the Pico low level code for communicating with the RP2040-zero via I2C.
+  The data is then sent to Serial for graphical visualization via python code. 
+*/
 
+// Used Libraries
 #include <Arduino.h>
 #include <Wire.h>
-/* #include <SPI.h>
+#include "VL53L1X.h"
+
+/* Possibly useful libraries 
+#include <SPI.h>
 #include "mcp2515.h"
 #include "CanWrapper.h"
-
 #include "include/definitions.h"
 #include "include/mod_config.h"
 #include "include/communication.h"
+#include "Adafruit_VL53L1X.h"
 */
-//#include "Adafruit_VL53L1X.h"
-#include "VL53L1X.h"
 
+// Definition of constants for comunication and data dimension:
 #define MATRIX_SIZE 4
-
 #define SDA_PIN_ZERO 2
 #define SCL_PIN_ZERO 3
 
-//int received_matrix[MATRIX_SIZE];
-
+// Number of sensors used:
 const uint8_t sensorCount = 2;
 
 int received_matrix[sensorCount][MATRIX_SIZE];
 
-//const size_t BYTES_FROM_ZERO = sizeof(received_matrix);
-
+// Dimension of received matrix:
 const size_t BYTES_FROM_ZERO = sizeof(received_matrix);
 
+// Comunication constants:
 const int ZERO_ADDRESS = 0x42;
 const long BAUD_RATE = 100000;
 
-  int fake_matrix[2][4] = {
-    {100, 200, 300, 400},
-    {500, 600, 700, 800}
-  };
-
-//TwoWire pico2zero_Bus(i2c1, SDA_PIN_ZERO, SCL_PIN_ZERO);
-
 void setup(){
-  //while (!Serial) {}
+  //while (!Serial) {}    // Just for debugging
   Serial.begin(115200);
  
+  // Possible pull up internal resistors for higher clock comunication:
   //pinMode(SDA_PIN_ZERO, INPUT_PULLUP);
   //pinMode(SCL_PIN_ZERO, INPUT_PULLUP);
 
+  // Initialization of I2C1 for comunication with RP2040-zero:
   Wire1.setSDA(2);  // GP4
   Wire1.setSCL(3);  // GP5
-  Wire1.begin();
+  Wire1.begin();    // initialized as Master
   Wire1.setClock(BAUD_RATE);
 
   Serial.println("Pico Master Initialized");
@@ -57,13 +54,16 @@ void setup(){
 }
 
 void loop(){
+  // Requesting bytes from RP:
   Wire1.requestFrom(ZERO_ADDRESS, BYTES_FROM_ZERO);
   
+  // Setting timeous and checking for available line of comunication:
   unsigned long timeout = millis();
   while (Wire1.available() < BYTES_FROM_ZERO && millis() - timeout < 100) {
     delay(1);
   }
   
+  // Using temporary buffer to save data:
   if (Wire1.available()>= BYTES_FROM_ZERO) {
     int tof_matrix[sensorCount][MATRIX_SIZE];
     byte temp_buffer[BYTES_FROM_ZERO];
@@ -72,16 +72,21 @@ void loop(){
       temp_buffer[i] = Wire1.read();
     }
 
+    // Converting raw data on buffer in a matrix of measured distances:
     memcpy(received_matrix, temp_buffer, BYTES_FROM_ZERO);
   
+    // Definition of an header for Python visualizer syncronization
+    // When the high level code receives the header, knows whose sensor the data are!
     byte header[4] = {0xAA, 0x55, 0xAA, 0x55};
     Serial.write(header, 4);
     Serial.write((const byte*)received_matrix, sizeof(received_matrix));  // This is just used to visualize it on python like depth camera
     Serial.flush();
 
     
-    //Serial.println("\nMaster: Matrix received successfully!");
+
     /*
+    // Used for debugging:
+
     for (size_t i = 0; i < sensorCount; i++) {
       for (size_t r = 0; r < MATRIX_SIZE; r++) {
         Serial.print(received_matrix[i][r]);
@@ -94,6 +99,9 @@ void loop(){
   else {
     Serial.println("Master: Incomplete data received.");
   }
-  */ } 
+  */
+ 
+} 
+  // Small delay that can be setted without much restriction.
   delay(50);
 }
